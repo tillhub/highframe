d7b4ee2f6a5cd2c170d981f6c564b2eacb89ef7b
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2019-07-22 18:20:06 +0200

    chore(packages): prepare vue package

---
+++ packages/highframe-vue/src/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="highframe-vue-container" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  created () {
+
+  }
+}
+</script>
